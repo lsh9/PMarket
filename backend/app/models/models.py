9a6431be46ce2d12bbbaa1e79f05e96d5f96a9e2
ZhaoChunshan <1900016633@pku.edu.cn>
2022-10-27 17:20:00 +0800

    补充填写ORM类

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -9,33 +9,44 @@ class Test(db.Model):
 
 class User(db.Model):
 	__tablename__ = "tb_user"
-	openid = db.Column(db.String(255), primary_key=True)
-	nickName = db.Column(db.String(255))
-	avatarUrl = db.Column(db.String(1023))
-	gender = db.Column(db.Integer)
-	contact = db.Column(db.String(255))
+	openid = db.Column(db.String(255), primary_key=True, nullable=False)
+	nickName = db.Column(db.String(255), nullable=False)
+	avatarUrl = db.Column(db.String(1023), nullable=False)
+	gender = db.Column(db.Integer, nullable=True)
+	contact = db.Column(db.String(255), nullable=True)
 	pass
 
 
 class Message(db.Model):
 	__tablename__ = "tb_message"
-	msg_id = db.Column(db.Integer, primary_key=True)
+	msgId = db.Column(db.Integer, primary_key=True, autoincrement = True, nullable = False)
+	description = db.Column(db.String(1023), nullable = False)
+	contact = db.Column(db.String(255), nullable = False)
+	pictureUrl = db.Column(db.String(255), nullable = False)
+	msgClass  = db.Column(db.Integer, nullable = False) # 商品大类别（1书籍，2日用，3数码）
 	pass
 
 
 class Goods(db.Model):
 	__tablename__ = "tb_goods"
-	msg_id = db.Column(db.Integer, primary_key=True)
+	msgId = db.Column(db.Integer, nullable = False)
+	goodsId = db.Column(db.Integer, nullable = False)
+	name = db.Column(db.String(255), nullable = False)
+	goodsClass  = db.Column(db.Integer, nullable = False) # 商品小类别（1日用，2数码，3数学，4计算机，5物理，6化学，…… n 其他）
+	price = db.Column(db.Numeric(10,2), nullable = False)
+	state  = db.Column(db.Integer, nullable = False) # 商品状态（1有货，2已有人要，3已售出）
 	pass
 
 
 class Release(db.Model):
 	__tablename__ = "tb_release"
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	userId = db.Column(db.Integer, nullable = False)
 	pass
 
 
 class Star(db.Model):
 	__tablename__ = "star"
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	userId = db.Column(db.Integer, nullable = False)
 	pass
