4f935a24e2d3ac6e0a215e1ec08a48c16873fa8a
rotom-washingmachine <1700012946@pku.edu.cn>
2022-10-29 02:18:07 +0800

    添加商品详细信息

--- PMarket-miniapp/pages/goods/goods-detail/index.js
+++ PMarket-miniapp/pages/goods/goods-detail/index.js
@@ -5,15 +5,90 @@ Page({
    * 页面的初始数据
    */
   data: {
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    goods: {},
+    isGoodsFavorite: false
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function (options) {
+    this.setData({
+      id: options.goodsId
+    })
+    this.getGoods(options.goodsId);
+    this.getComment(options.goodsId)
+    this.getCollect(options.goodsId)
 
   },
+
+  //查询商品详情
+  getGoods: function (goodsId) {
+    var that = this;
+    wx.request({
+      url: app.globalData.domain + '/api/goods/detail',
+      data: {
+        id: goodsId
+      },
+      success: function (res) {
+        that.setData({
+          goods: res.data.goods,
+        });
+      }
+    })
+  },
+    //收藏
+  collect(e) {
+    if (!wx.getStorageSync('token')) {
+      return;
+    }
+    var that = this;
+    var goodsId = this.data.goods.id;
+    var action = this.data.isGoodsFavorite ? 'uncollect' : 'save';
+    wx.request({
+      url: app.globalData.domain + '/api/collect/' + action,
+      data: {
+        goodsId: goodsId
+      },
+      header: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            isGoodsFavorite: !that.data.isGoodsFavorite
+          })
+        }
+      }
+    })
+  },
+
+  //是否收藏
+  getCollect(goodsId) {
+    var that = this;
+    wx.request({
+      url: app.globalData.domain + '/api/collect/isCollect',
+      data: {
+        goodsId: goodsId
+      },
+      header: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            isGoodsFavorite: res.data.isCollect
+          })
+        }
+      }
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
