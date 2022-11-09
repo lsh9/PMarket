dc4a7eef735b26d6e4738def700ecc4e03ad8130
Fu Jiawei <57051663+K331Mozart@users.noreply.github.com>
2022-11-09 22:13:56 +0800

    publish

--- PMarket-miniapp/pages/goods/publish/index.js
+++ PMarket-miniapp/pages/goods/publish/index.js
@@ -1,11 +1,19 @@
 // pages/goods/publish/index.js
-Page({
 
+const app = getApp()
+Page({
+  
   /**
    * 页面的初始数据
    */
   data: {
-
+    imageurl:null,
+    picUrls: [],
+    title: '',
+    opacity: '0.4',
+    width: '85',
+    position: 'center',
+    type: 1
   },
 
   /**
@@ -62,5 +70,117 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+uploadimage: function(e){
+    var that = this;
+    wx.chooseMedia({
+      count: 1,
+      mediaType: 'image',
+      sourceType: ['album', 'camera'],
+      success(res){
+        var tempFilePath = res.tempFiles[0].tempFilePath;
+        console.log(tempFilePath);
+        wx.uploadFile({
+          filePath: tempFilePath,
+          name: 'image',
+          url: app.globalData.domain + '/image/upload/goods',
+          success:function(res) {
+            console.log(res.data)
+            that.data.imageurl = res.data
+            wx.showModal({
+              title: '提示',
+              content: "图片上传成功",
+              showCancel: false
+            })
+          }
+        })
+      }
+    })
+},
+
+
+
+saveGoods: function(e) {
+  var that = this;
+  var name = e.detail.value.name;//获取商品名称
+  if (name == '') {
+    wx.showToast({
+      title: '请填写商品名称',
+      icon: 'none'
+    })
+    return;
   }
+
+  var category = e.detail.value.category;//获取商品名称
+  if (category == '') {
+    wx.showToast({
+      title: '填个012再走呗',
+      icon: 'none'
+    })
+    return;
+  }
+
+  var price = e.detail.value.price;//获取联系方式
+  if (price == '') {
+    wx.showToast({
+      title: '请填写价格',
+      icon: 'none'
+    })
+    return;
+  }
+
+  var contact = e.detail.value.contact;//获取商品价格
+  if (contact == '') {
+    wx.showToast({
+      title: '请留下联络方式',
+      icon: 'none'
+    })
+    return;
+  }
+
+  /*
+  if (that.data.picUrls.length == 0) {
+    wx.showToast({
+      title: '请上传商品图片',
+      icon: 'none'
+    })
+    return;
+  }
+  */
+
+  var description = e.detail.value.description;
+  if (description == '') {
+    wx.showToast({
+      title: '请填写商品描述',
+      icon: 'none'
+    })
+    return;
+  }
+  
+var goods = {
+  type: e.detail.value.type,//类型
+  name: name,//商品名称
+  description: description,//商品描述
+  category:category,//商品类型
+  price: price,//商品价格
+  contact:contact,//联系方式
+  userId:app.globalData.userId,//用户ID
+  pictureUrl:that.data.imageurl,//图片的URL
+};
+console.log(goods);
+wx.request({
+      url: app.globalData.domain + '/goods/publish',
+      method:'POST',
+      data: goods,
+      success:(res) =>{
+        console.log(res.data);
+        wx.showModal({
+          title: '提示',
+          content: "发布成功",
+          showCancel: false
+        })
+      }
+    })
+}
 })
