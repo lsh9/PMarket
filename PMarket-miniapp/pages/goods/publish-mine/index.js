1cbee20376c38f0bf756f091a2f1ab2d5464aa29
Xtuz <76842783+Xtuzzz@users.noreply.github.com>
2022-11-30 15:31:05 +0800

    添加我的发售界面

--- PMarket-miniapp/pages/goods/publish-mine/index.js
+++ PMarket-miniapp/pages/goods/publish-mine/index.js
@@ -1,66 +1,99 @@
-// pages/goods/publish-mine/index.js
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsList: [],
+    isLoad: false,
+    page: 1,
+    pageSize: 4,
+    goodsNum: -1,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function () {
+    this.getReleaseGoods();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+  onShow: function () {},
 
+
+
+  getReleaseGoods: function () {
+    var that = this;
+    wx.request({
+      url: app.globalData.domain + '/goods/getReleaseGoods',
+      method: "GET",
+      data: {
+        id: app.globalData.userId,
+        beginNo: that.data.goodsNum,
+        number: that.data.pageSize
+      },
+      success: function (res) {
+        if (that.data.page == 1) { //判断是否第一页
+          that.setData({
+            goodsList: []
+          });
+        }
+        if (res.data.length == 0) {
+          that.setData({
+            isLoad: true
+          });
+          return;
+        }
+        var goods = that.data.goodsList;
+        for (var i = 0; i < res.data.length; i++) {
+          goods.push(res.data[i]);
+        }
+        that.setData({
+          goodsList: goods,
+          isLoad: false
+        });
+        var noNow = res.data[res.data.length - 1].goodsId; //设置目前得到的最小商品号
+        that.setData({
+          goodsNum: noNow
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  //上拉加载
+  loadMore: function () {
+    console.log("load more")
+    var that = this;
+    var isLoad = this.data.isLoad;
+    console.log(isLoad)
+    if (!isLoad) {
+      this.setData({
+        page: that.data.page + 1
+      });
+      this.getReleaseGoods();
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  //下拉加载
+  onPullDownRefresh: function () {
+    this.setData({
+      page: 1,
+      goodsNum: -1
+    });
+    wx.showNavigationBarLoading()
+    this.getReleaseGoods();
+    setTimeout(function () {
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1000);
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+  //分享
+  onShareAppMessage: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onReachBottom: function () {
+    this.loadMore();
   }
 })
